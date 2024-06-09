efb13a9081365c7bd918fc998ccaee98427fb33b
daylerjeff199906 <daylersan@gmail.com>
2024-06-08 20:15:26 -0500

    fixed details

---
+++ src/app/(admin)/admin/layout.tsx
@@ -0,0 +1,27 @@
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ResizablePanelGroup
+      direction="horizontal"
+      className="rounded-lg border"
+    >
+      <ResizablePanel
+        defaultSize={25}
+        minSize={16}
+      >
+        <div className="flex h-full items-center justify-center p-6">
+          <span className="font-semibold">Sidebar</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={75}>
+        <div className="flex items-center justify-center p-6 ">{children}</div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  )
+}
